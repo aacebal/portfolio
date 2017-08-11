4a0188b9fcfd117e6621a8898321798dbe82dddf
Adel Acebal <adelacebal@yahoo.com>
2017-08-11 19:41:50 -0400

    adding animation

--- angular-portfolio/src/app/app.component.ts
+++ angular-portfolio/src/app/app.component.ts
@@ -1,17 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger(
+      'enterAnimation', [
+        transition(':enter', [
+          style({transform: 'translateX(100%)', opacity: 0}),
+          animate('500ms', style({transform: 'translateX(0)', opacity: 1}))
+        ]),
+        transition(':leave', [
+          style({transform: 'translateX(0)', opacity: 1}),
+          animate('500ms', style({transform: 'translateX(100%)', opacity: 0}))
+        ])
+      ]
+    )
+  ],
 })
 export class AppComponent {
   title = 'app';
 
-  nameReveal: boolean = false;
+  show:boolean = false;
 
   constructor(){ }
 
+  showFunction() {
+    this.show = !this.show;
+  }
 
+  @HostListener("window:scroll", [])
+onWindowScroll() {
+ //we'll do some stuff here when the window is scrolled
+ this.showFunction();
+}
 
 }
